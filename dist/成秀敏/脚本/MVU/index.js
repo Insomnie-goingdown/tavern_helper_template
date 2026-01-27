b989c061115f51d7bf6e793f0e7ab85d48ba3931
Insomnie-goingdown <330783497@qq.com>
2026-01-27 17:13:41 +0800

    Publish status bar

--- dist/成秀敏/脚本/MVU/index.js
+++
@@ -1,9 +0,0 @@
-import "https://testingcf.jsdelivr.net/gh/MagicalAstrogy/MagVarUpdate/artifact/bundle.js";
-
-;// external "https://testingcf.jsdelivr.net/gh/MagicalAstrogy/MagVarUpdate/artifact/bundle.js"
-
-;// ./src/成秀敏/脚本/MVU/index.ts
-
-
-
-//# sourceMappingURL=index.js.map
--- dist/角色卡示例/脚本/MVU/index.js
+++
@@ -1,9 +0,0 @@
-import "https://testingcf.jsdelivr.net/gh/MagicalAstrogy/MagVarUpdate/artifact/bundle.js";
-
-;// external "https://testingcf.jsdelivr.net/gh/MagicalAstrogy/MagVarUpdate/artifact/bundle.js"
-
-;// ./示例/角色卡示例/脚本/MVU/index.ts
-
-
-
-//# sourceMappingURL=index.js.map
